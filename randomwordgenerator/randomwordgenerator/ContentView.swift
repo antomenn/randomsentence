b227d2b62974a97199ec89d96ec5b407a6270357
Antonio Mennillo <58512129+antomenn@users.noreply.github.com>
2022-03-15 15:23:35 +0100

    Add files via upload

---
+++ randomwordgenerator/randomwordgenerator/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  randomwordgenerator
+//
+//  Created by Antonio Mennillo on 11/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
---
+++ randomwordgenerator/randomwordgenerator/randomwordgeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomwordgeneratorApp.swift
+//  randomwordgenerator
+//
+//  Created by Antonio Mennillo on 11/03/22.
+//
+
+import SwiftUI
+
+@main
+struct randomwordgeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
